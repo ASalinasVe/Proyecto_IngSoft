5da3a4a8b963da25ef0928f242f433fcf3988a5e
unknown <huascar.rivero@ucb.edu.bo>
2025-04-24 09:57:20 -0400

    rebase

---
+++ src/registro-usuario.test.js
@@ -0,0 +1,8 @@
+import { Usuario } from './registro-usuario'
+
+describe('Registro de usuarios', () => {
+    test('deberia crear el usuario correctamente', () => {
+        let usuario = new Usuario('Ana', 1234, 'pass1234');
+        expect(usuario).toEqual({name: 'Ana', v_id: 1234, password: 'pass1234'});
+    });
+});
