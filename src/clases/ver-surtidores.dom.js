a0dc366e705a4f3347e41fa3207533aab53acf79
ASalinasVe <alejandra.salinas@ucb.edu.bo>
2025-05-07 10:36:39 -0400

    feat, ver-surtidores en index.html, corregido

--- src/clases/ver-surtidores.dom.js
+++ src/clases/ver-surtidores.dom.js
@@ -1,14 +1,14 @@
 import { obtenerSurtidores } from './surtidor.js';
 
 document.addEventListener("DOMContentLoaded", () => {
-  const lista = document.getElementById("listaSurtidores");
+  const lista = document.getElementById("lista-surtidores");
 
   if (lista) {
     const surtidores = obtenerSurtidores();
 
     surtidores.forEach(s => {
       const li = document.createElement("li");
-      li.textContent = `ID: ${s.id} - ${s.nombre}, Ubicación: ${s.ubicacion}, Tipo: ${s.tipo}, Precio: ${s.precio}, Cantidad: ${s.cantidad}`;
+      li.textContent = `Surtidor: ${s.nombre}, Ubicación: ${s.ubicacion}, Tipo: ${s.tipo}, Precio: ${s.precio}, Cantidad: ${s.cantidad}, Disponible: ${s.disponible}`;
       lista.appendChild(li);
     });
   }
