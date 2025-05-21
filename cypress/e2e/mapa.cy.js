8ced72e225aba4f292853d774bd15c6d7e72dd32
Huascar Rivero <huascar.rivero@ucb.edu.bo>
2025-05-21 15:27:51 -0400

    feat: geocodificacion de ubicacion y agregacion al mapa

--- cypress/e2e/mapa.cy.js
+++ cypress/e2e/mapa.cy.js
@@ -8,4 +8,15 @@ describe("Pagina de Mapa", () => {
     cy.get('.leaflet-marker-icon').first().click();
     cy.get('.leaflet-popup-content').should('contain.text', 'Hola desde la UCB');
   });
+
+  it('deberia mostrar el maca con un marcador obtenido por geocodificacion', () => {
+    
+    cy.visit("/componentes/mapa/mapa.html");
+    cy.get('#map').should('exist');
+    cy.wait(1000); // En caso de que el mapa tarde en cargar
+
+    cy.get('.leaflet-marker-icon').should('have.length.at.least', 1);
+    cy.get('.leaflet-marker-icon').last().click();
+    cy.get('.leaflet-popup-content').should('contain.text', 'LA CATOLICA');
+  })
 });
--- src/clases/mapa.dom.js
+++ src/clases/mapa.dom.js
@@ -1,9 +1,37 @@
-const map = L.map('map').setView([-34.6037, -58.3816], 13);
+const map = L.map('map').setView([-17.37, -66.14], 4);
+
+async function geocodificarUbicacion (ubicacion) {
+  const url = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(ubicacion)}`;
+  const respuesta = await fetch(url);
+  const datos = await respuesta.json();
+
+  if (datos.length > 0) {
+    return {
+      lat: parseFloat(datos[0].lat),
+      lon: parseFloat(datos[0].lon)
+    };
+  } else {
+    throw new Error("No se encontró la ubicación: " + direccion);
+  }
+}
+
+async function agregarUbicacionMapa (ubicacion, mapa) {
+  try {
+    const coordenadas = await geocodificarUbicacion(ubicacion);
+    console.log(coordenadas);
+    const marcador = L.marker([coordenadas.lat, coordenadas.lon]).addTo(mapa);
+    const popUp = 'LA CATOLICA';
+    marcador.bindPopup(popUp);
+  } catch (e) {
+    console.log('Error al intentar geocodificar:', ubicacion, e);
+  }
+}
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-L.marker([-34.6037, -58.3816]).addTo(map)
-    .bindPopup('¡Hola desde la UCB!');
+L.marker([-34.6037, -58.3816]).addTo(map).bindPopup('¡Hola desde la UCB!');
+
+agregarUbicacionMapa('universidad catolica boliviana san pablo cochabamba', map);
