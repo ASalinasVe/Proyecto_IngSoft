288407fb0c35b1c20709e77626c8a8de78abdd94
Huascar Rivero <huascar.rivero@ucb.edu.bo>
2025-05-21 16:27:08 -0400

    refactor: trabajar con listas de gasolineras y no solo ubicaciones

--- src/clases/mapa.dom.js
+++ src/clases/mapa.dom.js
@@ -1,6 +1,6 @@
 const map = L.map('map').setView([-17.37, -66.14], 4);
 
-async function geocodificarUbicacion (ubicacion) {
+async function geocodificarUbicacion(ubicacion) {
   const url = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(ubicacion)}`;
   const respuesta = await fetch(url);
   const datos = await respuesta.json();
@@ -15,23 +15,25 @@ async function geocodificarUbicacion (ubicacion) {
   }
 }
 
-async function agregarUbicacionMapa (ubicacion, mapa) {
-  try {
-    const coordenadas = await geocodificarUbicacion(ubicacion);
-    console.log(coordenadas);
-    const marcador = L.marker([coordenadas.lat, coordenadas.lon]).addTo(mapa);
-    const popUp = 'LA CATOLICA';
-    marcador.bindPopup(popUp);
-  } catch (e) {
-    console.log('Error al intentar geocodificar:', ubicacion, e);
+async function agregarMarcadorGasolinerasMapa(gasolineras, mapa) {
+  for (const gasolinera of gasolineras) {
+    try {
+      const coordenadas = await geocodificarUbicacion(gasolinera.ubicacion);
+      console.log(coordenadas);
+      const marcador = L.marker([coordenadas.lat, coordenadas.lon]).addTo(mapa);
+      const popUp = 'LA CATOLICA';
+      marcador.bindPopup(popUp);
+    } catch (e) {
+      console.log('Error al intentar geocodificar:', ubicacion, e);
+    }
   }
 }
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
 L.marker([-34.6037, -58.3816]).addTo(map).bindPopup('¡Hola desde la UCB!');
 
-agregarUbicacionMapa('universidad catolica boliviana san pablo cochabamba', map);
+agregarMarcadorGasolinerasMapa([{ id: 1, nombre: "Surtidor A", ubicacion: "universidad catolica boliviana san pablo cochabamba"}], map);
