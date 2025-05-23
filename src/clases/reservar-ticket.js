3f8eb328898747580ec7690dd90c48ab80de168e
AldrinZurita <axzt2004@gmail.com>
2025-05-22 21:06:03 -0400

    fix: funcionalidad para la asignación de número de ticket usando localStorage

--- src/clases/reservar-ticket.js
+++ src/clases/reservar-ticket.js
@@ -1,8 +1,17 @@
+function obtenerTickets() {
+  const data = localStorage.getItem("tickets");
+  return data ? JSON.parse(data) : [];
+}
+
 function crearTicket(entrada) {
+  const tickets = obtenerTickets();
+  const numero = tickets.length + 1;
+
   return {
     placa: entrada.placa,
-    gasolinera: entrada.gasolinera
+    gasolinera: entrada.gasolinera,
+    numero
   };
 }
 
-export { crearTicket };
+export { crearTicket, obtenerTickets };
